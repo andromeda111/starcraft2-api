a8a3d4b5acd5de4b48f2e32db4847d817859f55d
John <jmseymour111@gmail.com>
2017-06-23 15:43:22 -0700

    added trust proxy

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ hbs.registerPartials(__dirname + '/views/partials')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.enable('trust proxy')
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
